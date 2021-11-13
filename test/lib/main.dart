0c84a5577ea93385ed6d65f02e7f0e895751736b
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2021-11-13 20:36:53 +0300

    Changed Title

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Test',
       theme: ThemeData(
         // This is the theme of your application.
         //
